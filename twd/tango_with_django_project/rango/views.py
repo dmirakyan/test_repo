18b14f30fc267b314587e2d86c2c64526559da56
dmirakyan <damirakyan@gmail.com>
2020-04-06 08:43:58 -0700

    chapter 3 twd
    chapter 3

--- twd/tango_with_django_project/rango/views.py
+++ twd/tango_with_django_project/rango/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = HttpResponse()
+    response.write("Rango says hey there partner! </br>")
+    response.write("<a href='about'>about this site</a>")
+    # <a href="url">link text</a>
+    return response
+
+def about(request):
+    response = HttpResponse()
+    response.write('Rango says here is the about page </br>')
+    response.write("<a href='/rango/'>home</a>")
+    return response
+
 
 # Create your views here.
