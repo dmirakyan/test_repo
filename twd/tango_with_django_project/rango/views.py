5ce361b9254a07f5c2b915656c85fd057111e0fa
dmirakyan <damirakyan@gmail.com>
2020-04-08 19:30:44 -0700

    ch 4 complete

--- twd/tango_with_django_project/rango/views.py
+++ twd/tango_with_django_project/rango/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    response = HttpResponse()
-    response.write("Rango says hey there partner! </br>")
-    response.write("<a href='/rango/about/'>About</a>")
-    # <a href="url">link text</a>
-    return response
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to the {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy cookie, candy, cupcake!'}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
-    response = HttpResponse()
-    response.write('Rango says here is the about page. </br>')
-    response.write("<a href='/rango/'>Index</a>")
-    return response
+    context_dict = {'boldmessage': 'This tutorial has been put together by Banksy'}
+    return render(request,'rango/about.html',context=context_dict)
+
+    # response = HttpResponse()
+    # response.write('Rango says here is the about page. </br>')
+    # response.write("<a href='/rango/'>Index</a>")
+    # return response
 
 
 # Create your views here.
